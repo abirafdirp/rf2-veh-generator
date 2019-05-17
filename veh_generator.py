243900427a41008820fbcaf08636f3527ddf36ed
Abirafdi Raditya Putra <abirafdiraditya@gmail.com>
2019-05-17 17:22:49 +0700

    Revamp logic

--- veh_generator.py
+++ veh_generator.py
@@ -6,8 +6,8 @@ import shutil
 with open('base.VEH') as f:
     base_veh = f.read()
 
-drivers_data = open('data.csv')
-csv_reader = csv.DictReader(drivers_data)
+drivers_data_csv = open('data.csv')
+csv_reader = csv.DictReader(drivers_data_csv)
 
 if not os.path.exists('output'):
     os.mkdir('output')
@@ -19,52 +19,58 @@ else:
 
 input_dir = os.fsencode('input')
 
+
+dds_data = {}
 for file in os.listdir(input_dir):
     file_name_with_ext = os.fsdecode(file)
 
     if not file_name_with_ext.endswith('.dds'):
-        print(f'invalid file present in input directory: {file_name_with_ext}')
         continue
 
     file_name = file_name_with_ext.split('.')[0]
 
     # must follow this format xx_alt.dds
     car_number = file_name.split('_')[0]
-    is_guest = True if car_number == 'guest' else False
-    if not car_number.isdigit() and not is_guest:
-        print(
-            f'invalid dds file name_in_data format, must be xx_alt.dds: '
-            f'{file_name_with_ext}'
-        )
+
+    # there will be 'guest' as car number
+    dds_data[car_number] = {
+        'file_name': file_name,
+        'file_name_with_ext': file_name_with_ext
+    }
+
+drivers_data = {}
+for position, line in enumerate(csv_reader):
+    if position == 1:
         continue
 
-    for position, line in enumerate(csv_reader):
-        if position == 1:
-            continue
+    number_in_csv = line['NUMBER']
 
-        number_in_data = line['NUMBER']
-        if not number_in_data == car_number:
-            continue
+    if number_in_csv not in dds_data:
+        print(f'car number {number_in_csv} present in CSV but does not have DDS file')  # noqa
+        continue
 
-        is_guest_in_data = True if line['GUEST'] != '' else False
-        name_in_data = line['NAME']
-        team_in_data = line['TEAM']
-        class_in_data = line['CLASS']
+    is_guest_in_data = True if line['GUEST'] != '' else False
 
-        with open(f'output/{file_name}.VEH', 'w+') as f:
-            veh = base_veh.replace('{NAME}', name_in_data)
-            veh = veh.replace('{TEAM}', team_in_data)
-            veh = veh.replace('{CLASS}', class_in_data)
-            veh = veh.replace('{NUMBER}', number_in_data)
+    name_in_data = line['NAME']
+    team_in_csv = line['TEAM']
+    class_in_csv = line['CLASS']
 
-            if is_guest_in_data:
-                veh = veh.replace('{DDS}', 'guest_alt.dds')
-            else:
-                veh = veh.replace('{DDS}', f'{car_number}_alt.dds')
+    file_name = dds_data[number_in_csv]['file_name']
+    file_name_with_ext = dds_data[number_in_csv]['file_name_with_ext']
 
-            f.write(veh)
-            shutil.copy(f'input/{file_name_with_ext}', f'output/{file_name_with_ext}')  # noqa
-            print(f'{file_name} VEH generated')
-    drivers_data.seek(0)
+    with open(f'output/{file_name}.VEH', 'w+') as f:
+        veh = base_veh.replace('{NAME}', name_in_data)
+        veh = veh.replace('{TEAM}', team_in_csv)
+        veh = veh.replace('{CLASS}', class_in_csv)
+        veh = veh.replace('{NUMBER}', number_in_csv)
 
-drivers_data.close()
+        if is_guest_in_data:
+            veh = veh.replace('{DDS}', 'guest_alt.dds')
+        else:
+            veh = veh.replace('{DDS}', f'{number_in_csv}_alt.dds')
+
+        f.write(veh)
+        shutil.copy(f'input/{file_name_with_ext}', f'output/{file_name_with_ext}')  # noqa
+        print(f'{file_name} VEH generated')
+
+drivers_data_csv.close()
